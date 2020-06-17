257e0c3f7991b56975a234c024d96b6e1bdc72a5
James <james.kiesel@gmail.com>
2020-06-17 19:50:34 +1200

    Remove settings, add translations for resume link

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,4 @@
 # name: debaleena-discourse
 # about: Debaleena Discourse
 # version: 0.1
-enabled_site_setting :redirect_on_profile_picture
-enabled_site_setting :redirect_avataar_external_website
-
 register_asset "stylesheets/stemaway-profile-link.css"
