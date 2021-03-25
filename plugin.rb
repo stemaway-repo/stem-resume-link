8bd91c5f361d25d242ad9cb28ae346005923cc85
Keegan George <kgeorge13@gmail.com>
2021-03-25 09:58:39 -0700

    Change name and add enabler setting

--- plugin.rb
+++ plugin.rb
@@ -1,4 +1,6 @@
-# name: debaleena-discourse
-# about: Debaleena Discourse
-# version: 0.1
+# name: stemaway-resume-link
+# about: STEM-Away Resume Link
+# version: 1.0
+
+enabled_site_setting :resume_link_enabled
 register_asset "stylesheets/stemaway-profile-link.css"
