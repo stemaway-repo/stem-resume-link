0603b2151d55848b207e9b789c49ac25cae9d119
Keegan George <kgeorge13@gmail.com>
2021-03-25 10:04:25 -0700

    Add git repo to settings

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,7 @@
 # name: stemaway-resume-link
 # about: STEM-Away Resume Link
-# version: 1.0
+# version: 1.1.0
+# url: https://github.com/stemaway-repo/stem-resume-link
 
 enabled_site_setting :resume_link_enabled
 register_asset "stylesheets/stemaway-profile-link.css"
